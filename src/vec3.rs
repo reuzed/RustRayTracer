52d267cebca917a700b5bed0ba36a9609b1bb62e
Neel Maniar <neelmaniar02@gmail.com>
2026-01-10 19:30:30 +0000

    First Vec3 bit
    Co-authored-by: reuzed <reuzed@users.noreply.github.com>

---
+++ src/vec3.rs
@@ -0,0 +1,27 @@
+use std::fmt{}
+
+#[derive(Copy, Clone, Default)]
+pub struct Vec3 {
+    e: [f64; 3]
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 {e: [x,y,z]}
+    }
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
+}
+
+// Type alias
+pub type Point3 = Vec3;

b3f6c3f2865653711faa78cf66d501c2654d8bac
reuzed <49994279+reuzed@users.noreply.github.com>
2026-01-10 19:14:33 +0000

    eprint

--- examples/first_image.rs
+++ examples/first_image.rs
@@ -9,6 +9,7 @@ fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("\r Scanlines remaining {}", j);
         for i in (0..IMAGE_WIDTH).rev() {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -21,4 +22,5 @@ fn main() {
             print!("{} {} {}\n", ir, ig, ib);
         }   
     } 
+    eprintln!("Done.")
 }

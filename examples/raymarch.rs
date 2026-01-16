251d4f6371e2910f5d72252b1cfeab3339145ebc
reuzed <49994279+reuzed@users.noreply.github.com>
2026-01-16 16:37:26 +0000

    alter shadow calculation in raymarch

--- examples/raymarch.rs
+++ examples/raymarch.rs
@@ -1,5 +1,7 @@
+use std::io;
+
 use rust_ray_tracer::{
-    camera::{Camera, Renderer}, constants::INFINITY, hittable::{HitRecord, Hittable}, hittable_list::HittableList, linalg::vec3::{Point3, Vec3, dot, unit_vector}, ray::Ray, raymarching::{march, repetition, rotate, sd_box, sd_plane, sd_sphere, smooth_union, translate, union}, shading::{Color, shade}, sphere::Sphere
+    camera::{Camera, Renderer}, constants::INFINITY, hittable::{HitRecord, Hittable}, hittable_list::HittableList, linalg::vec3::{Point3, Vec3, dot, unit_vector}, random::random_double, ray::Ray, raymarching::{march, repetition, rotate, sd_box, sd_plane, sd_sphere, smooth_union, translate, union}, shading::{Color, shade, write_color}, sphere::Sphere
 };
 
 fn main() {
@@ -20,41 +22,63 @@ fn main() {
         16.0 / 9.0,
     );
 
-    let renderer = Renderer::new(512, 16.0 / 9.0, camera);
+    let renderer = Renderer::new(512, 16.0 / 9.0, camera.clone());
 
     // Render
 
-    let sdf = {
-        let sdf1 = sd_sphere(0.5);
-        let sdf1 = translate(sdf1, Vec3::new(0.0, 0.0, -1.0));
-        let sdf2 = sd_box(Vec3::new(0.3, 0.3, 0.3));
-        let sdf2 = translate(sdf2, Vec3::new(1.0, 0.0, -1.5));
-        let sdf2 = rotate(sdf2, 5.0, -20.0, 50.0);
-        let sdf_floor = sd_plane(Vec3::new(0.0, 1.0, 0.0), 0.0);
-        smooth_union(sdf_floor, union(repetition(sdf1, 5.0), sdf2), 0.2)
-    };
+    // let sdf = {
+    //     let sdf1 = sd_sphere(0.5);
+    //     let sdf1 = translate(sdf1, Vec3::new(0.0, 0.0, -1.0));
+    //     let sdf2 = sd_box(Vec3::new(0.3, 0.3, 0.3));
+    //     let sdf2 = translate(sdf2, Vec3::new(1.0, 0.0, -1.5));
+    //     let sdf2 = rotate(sdf2, 5.0, -20.0, 50.0);
+    //     let sdf_floor = sd_plane(Vec3::new(0.0, 1.0, 0.0), 0.0);
+    //     smooth_union(sdf_floor, union(repetition(sdf1, 5.0), repetition(sdf2, 20.0)), 0.2)
+    // };
 
+    let sdf = {
+        let sdf_floor = sd_plane(Vec3::new(0.0, 1.0, 0.0), 0.0);
+        let sdf_box = sd_box(Vec3::new(0.3, 2.3, 0.3));
+        let sdf_box = translate(sdf_box, Vec3::new(0.0, 2.5, 0.0));
+        union(sdf_floor, sdf_box)
+    };
+    
     let light = Point3::new(1.0, 4.0, 2.0);
 
-    renderer.render_to_ppm(|ray| {
-        let res = march(ray, sdf.clone());
 
-        if res.hit {
-            let hr = res.hr.unwrap();
+    const SAMPLES_PER_PIXEL: i32 = 12;
 
-            let to_light_vec = unit_vector(light - hr.pos);
-            let to_light = Ray::new(hr.pos + 0.1 * to_light_vec, to_light_vec);
-            let shadow_res = march(to_light, sdf.clone());
-            if shadow_res.hit {
-                Color::new(0.1,0.1,0.1)
+    print!("{}", renderer.ppm_header());
+
+    for j in (0..renderer.image_height).rev(){
+        for i in (0..renderer.image_width).rev(){
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + random_double()) / (renderer.image_width - 1) as f64;
+                let v = (j as f64 + random_double()) / (renderer.image_height - 1) as f64;
+
+                let ray = camera.get_ray(u, v);
+
+                let res = march(ray, sdf.clone());
+
+                if res.hit {
+                    let hr = res.hr.unwrap();
+
+                    let to_light_vec = unit_vector(light - hr.pos);
+                    let to_light = Ray::new(hr.pos + 0.0001 * to_light_vec, to_light_vec);
+                    let shadow_res = march(to_light, sdf.clone());
+                    if shadow_res.hit {
+                        pixel_color += 0.2 * Color::new(1.0,0.3,0.5) * dot(unit_vector(light - hr.pos), hr.normal)
+                    }
+                    else {
+                        pixel_color += Color::new(1.0,0.3,0.5) * dot(unit_vector(light - hr.pos), hr.normal)
+                    }
+                }
+                else {
+                    pixel_color += Color::new(0.2,0.3,0.5)
+                }
             }
-            else {
-                Color::new(1.0,0.3,0.5) * dot(unit_vector(light - hr.pos), hr.normal)
-            }
+            write_color(&mut io::stdout(), pixel_color, SAMPLES_PER_PIXEL);
         }
-        else {
-            Color::new(0.2,0.3,0.5)
-        }
-
-    })
+    }
 }
--- src/raymarching/raymarching.rs
+++ src/raymarching/raymarching.rs
@@ -1,5 +1,7 @@
 // Functions to march rays and see what happens
 
+use std::f64::consts::PI;
+
 use crate::{
     ray::Ray,
     raymarching::{Sdf, normal},
@@ -25,6 +27,7 @@ pub fn march_simple(ray: Ray, sdf: impl Sdf) -> bool {
 }
 
 pub struct HitResult {
+    pub travelled: f64,
     pub distance: f64,
     pub pos: Vec3,
     pub normal: Vec3,
@@ -36,24 +39,48 @@ pub struct MarchResult {
     pub hr: Option<HitResult>,
 }
 
+const SHADOW_STEPS: i32 = 256;
+const SHADOW_EPSILON: f64 = 0.001;
+
+pub fn softshadow(ray: Ray, mint: f64, maxt: f64, sdf: impl Sdf, penumbra_sharpness: f64) -> f64 {
+    let mut light: f64 = 1.0;
+    let mut t = mint;
+    for _ in 0..SHADOW_STEPS {
+        if t > maxt {
+            break;
+        }
+        let p = ray.at(t);
+        let h = sdf(p);
+        if h < SHADOW_EPSILON {
+            return 0.0;
+        }
+        t += h;
+        light = light.min(penumbra_sharpness * h / t);
+    }
+    light
+}
+
 pub fn march(ray: Ray, sdf: impl Sdf) -> MarchResult {
     // Take a ray and sdf, march up to some tolerance or max number of steps
     let mut pos = ray.origin();
+    let mut travelled = 0.0;
     let unit_dir = unit_vector(ray.direction());
     let mut steps_taken = 0;
     for i in 0..MAX_STEPS {
         let d = sdf(pos);
-        if d < CLOSE_TOLERANCE {
+        if d < CLOSE_TOLERANCE && travelled > 3.0 * CLOSE_TOLERANCE {
             return MarchResult {
                 hit: true,
                 steps: i,
                 hr: Some(HitResult{
+                    travelled: travelled,
                     distance: d,
                     pos: pos,
                     normal: normal(sdf, pos),
                 })
             };
         }
+        travelled += d;
         pos += unit_dir * d;
 
         steps_taken += 1;

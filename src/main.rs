4e151797aff17f936912b7f7b86c6dc49ef4e50f
reuzed <49994279+reuzed@users.noreply.github.com>
2026-01-18 19:53:59 +0000

    diffuse material bouncing
    Co-authored-by: neel-maniar <neel-maniar@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use std::io;
 
 use rust_ray_tracer::{
-    camera::{Camera, Renderer}, constants::INFINITY, hittable::{HitRecord, Hittable}, hittable_list::HittableList, linalg::vec3::{Point3, Vec3}, media::ppm::ppm_header, random::random_double, shading::{Color, shade, write_color}, sphere::Sphere
+    camera::{Camera, Renderer}, constants::INFINITY, hittable::{HitRecord, Hittable}, hittable_list::HittableList, linalg::vec3::{self, Point3, Vec3, dot, unit_vector}, media::ppm::ppm_header, random::random_double, ray::Ray, shading::{Color, shade, write_color}, sphere::Sphere
 };
+
+fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
+    let mut rec = HitRecord::new();
+
+    if depth <= 0 {
+        return Vec3::new(0.0, 0.0, 0.0);
+    }
+
+    // direction = rec.normal + vec3::random_in_unit_sphere();
+    if world.hit(&r, 0.0, INFINITY, &mut rec) {
+        let direction = rec.normal + vec3::random_in_unit_sphere(); 
+        return 0.5 * ray_color(&Ray::new(rec.p, direction), world, depth-1);      // bounce
+    }
+
+    // return sky col
+    let up = Vec3::new(0.0,1.0,0.0);
+    return dot(unit_vector(r.direction()), up).max(0.0) * Vec3::new(1.0,1.0,1.0)
+}
 
 fn main() {
     // World
@@ -27,7 +45,7 @@ fn main() {
     // Render
 
     const SAMPLES_PER_PIXEL: i32 = 100;
-
+    const MAX_DEPTH: i32 = 50;
     print!("{}", ppm_header(renderer.image_width, renderer.image_height));
 
     for j in (0..renderer.image_height).rev() {
@@ -36,11 +54,9 @@ fn main() {
             for _ in 0..SAMPLES_PER_PIXEL {
                 let u = (i as f64 + random_double()) / (renderer.image_width - 1) as f64;
                 let v = (j as f64 + random_double()) / (renderer.image_height - 1) as f64;
-                let mut rec = HitRecord::new();
+                let ray = camera.get_ray(u, v);
 
-                world.hit(&camera.get_ray(u, v), 0.0, INFINITY, &mut rec);
-
-                pixel_color += shade(rec)
+                pixel_color += ray_color(&ray, &world, MAX_DEPTH)
             }
             write_color(&mut io::stdout(), pixel_color, SAMPLES_PER_PIXEL);
         }
